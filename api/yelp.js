9d3679bf54a43a92033afaa6d83f0e4129b26fa6
Ömer Faruk Gündoğdu <omerrfarukgundogdu@gmail.com>
2024-08-29 15:15:42 +0300

    gitignore son güncelleme.

--- api/yelp.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-export default axios.create({
-    baseURL : "https://api.yelp.com/v3/businesses",
-    headers : {
-        Authorization : "Bearer GxG7BpdIGuikZ5VpFBrWV5KGorkKwHDxT31A3Lsw6EgBbUbTM5y-kHog51clm8Qt8N_P9HLmDdQe_MUjEHvOw1LuwLLrY2WhdqAtS3UUvWl1IcSDY7FPqtRg5TTQZnYx"
-    }
-})
